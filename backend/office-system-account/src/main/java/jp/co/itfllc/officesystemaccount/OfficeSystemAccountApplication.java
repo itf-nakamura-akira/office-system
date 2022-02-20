54991ba1c24216af76291760aa47f1f50f6519a4
itf-nakamura-akira <nakamura.akira@itfllc.co.jp>
2022-02-20 04:44:14 +0000

    Spring Boot Initializerでアカウント用サービスの追加

---
+++ backend/office-system-account/src/main/java/jp/co/itfllc/officesystemaccount/OfficeSystemAccountApplication.java
@@ -0,0 +1,13 @@
+package jp.co.itfllc.officesystemaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeSystemAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeSystemAccountApplication.class, args);
+	}
+
+}
---
+++ backend/office-system-account/src/test/java/jp/co/itfllc/officesystemaccount/OfficeSystemAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.itfllc.officesystemaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeSystemAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
